acdf018877c9d45462cf0b95a1158db3e9489559
Akshay C <chnakshay@gmail.com>
2021-11-27 06:35:29 -0600

    pi 1

---
+++ Assets/direcMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class direcMenu : MonoBehaviour
+{
+    public GameObject menu2;
+    
+    // Start is called before the first frame update
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void pullupMenu(){
+        if (menu2.active == true){
+            menu2.active = false;
+        }
+        else{
+            menu2.active = true;
+        }
+    }
+}
---
+++ Assets/rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    
+    public GameObject Rotobj; 
+    
+        
+    public void rotLeft(){
+        Rotobj.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
+    }
+    
+    public void rotRight(){
+        Rotobj.transform.Rotate(-90.0f, 0.0f, 0.0f, Space.Self);
+    }
+    
+    public void rotUp(){
+        Rotobj.transform.Rotate(0.0f, 0.0f, 0.0f, Space.Self);
+    }
+    
+    public void rotDown(){
+        Rotobj.transform.Rotate(180.0f, 0.0f, 0.0f, Space.Self);
+    }
+}
