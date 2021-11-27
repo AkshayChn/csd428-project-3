7d3b2b282358f15438f67a8a97b7a014b31c62b1
Akshay C <chnakshay@gmail.com>
2021-11-27 09:51:46 -0600

    pi 1.8

--- Assets/rotate.cs
+++ Assets/rotate.cs
@@ -9,28 +9,36 @@ public class rotate : MonoBehaviour
     public GameObject Player;
     public GameObject RoomObj;
     
-        
+    
+
+
+
+      
     public void rotLeft(){
-        Rotobj.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
+        //Player.transform.position = RoomObj.transform.position; 
+        Rotobj.transform.Rotate(10.0f, 0.0f, 0.0f, Space.Self);
         //Player.transform.position = new Vector3(14, -2, 0);
-        Player.transform.position = RoomObj.transform.position; 
+        
     }
     
     public void rotRight(){
-        Rotobj.transform.Rotate(-90.0f, 0.0f, 0.0f, Space.Self);
+        //Player.transform.position = RoomObj.transform.position;
+        Rotobj.transform.Rotate(-10.0f, 0.0f, 0.0f, Space.Self);
 //Player.transform.position = new Vector3(14, -2, 0);
-        Player.transform.position = RoomObj.transform.position; 
+         
     }
     
     public void rotUp(){
+        Player.transform.position = RoomObj.transform.position; 
         Rotobj.transform.Rotate(0.0f, 0.0f, 0.0f, Space.Self);
 //Player.transform.position = new Vector3(14, -2, 0);
-        Player.transform.position = RoomObj.transform.position; 
+        
     }
     
     public void rotDown(){
+        Player.transform.position = RoomObj.transform.position; 
         Rotobj.transform.Rotate(180.0f, 0.0f, 0.0f, Space.Self);
 //Player.transform.position = new Vector3(14, -2, 0);
-        Player.transform.position = RoomObj.transform.position; 
+        
     }
 }
