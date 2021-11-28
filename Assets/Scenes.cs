015bfcb03e9e072f80da5095c74d34e2e1cb699e
Akshay C <chnakshay@gmail.com>
2021-11-27 20:06:53 -0600

    pi 2.1

---
+++ Assets/Scenes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public void loadProject2(){
+        SceneManager.LoadScene("Project2");
+    }
+    
+    public void loadMego(){
+        SceneManager.LoadScene("MegoPlayset_old");
+    }
+    
+    public void loadGiant(){
+        SceneManager.LoadScene("LandOfGiants");
+    }
+    
+    public void loadDancing(){
+        SceneManager.LoadScene("dancing");
+    }
+    
+    //public void loadZero(){
+    //    SceneManager.LoadScene("ZeroGravity");
+    //}
+}
